01e14abf3123a9a6a2713bfd81f009467862103e
Reyhaneh <reyhaneh.sa.mo@gmail.com>
2025-02-04 23:03:32 +0330

    add js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+const drag= (e) => {
+    e.dataTransfer.setData("text", e.target.id);
+}
 
+const drop= (e) => {
+    e.preventDefault();
+    let data= e.dataTransfer.getData("text");
+    
+    const parentItem= e.target;
+    parentItem.append(document.getElementById(data))
+}
+
+const allowDrop= (e) => {
+    e.preventDefault();
+}
